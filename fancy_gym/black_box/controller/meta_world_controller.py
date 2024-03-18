fix the incorrect position scaling and control scaling

fancy_gym/black_box/controller/meta_world_controller.py
@@ -21,5 +21,5 @@ class MetaWorldController(BaseController):
         if xyz_pos.shape != cur_pos.shape:
             raise ValueError(f"Mismatch in dimension between desired position"
                              f" {xyz_pos.shape} and current position {cur_pos.shape}")
-        trq = np.hstack([(xyz_pos - cur_pos), gripper_pos])
+        trq = np.hstack([(xyz_pos - cur_pos) * 100, gripper_pos])
         return trq

fancy_gym/meta/base_metaworld_mp_wrapper.py
@@ -60,7 +60,7 @@ class BaseMetaworldMPWrapper(RawInterfaceWrapper):
     @property
     def current_pos(self) -> Union[float, int, np.ndarray]:
         r_close = self.env.data.joint('r_close').qpos
-        return np.hstack([self.env.data.mocap_pos.flatten() / self.env.action_scale, r_close])
+        return np.hstack([self.env.data.mocap_pos.flatten(), r_close])
 
     @property
     def current_vel(self) -> Union[float, int, np.ndarray, Tuple]: